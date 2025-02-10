cf5c47a7bc4f4d9c99a983d5b172a666b0c249a4
jesusxambro <jesusxambro@gmail.com>
2025-02-09 20:21:54 -0600

    added multiplication

--- 1/my_module.py
+++ 1/my_module.py
@@ -3,3 +3,7 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
+
+def multiply(x, y=1):
+    return x * y
+
--- 1/test_my_module.py
+++ 1/test_my_module.py
@@ -1,5 +1,5 @@
 import unittest
-from my_module import add, subtract
+from my_module import add, subtract, multiply
 
 class TestMyModule(unittest.TestCase):
 
@@ -10,6 +10,10 @@ class TestMyModule(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(1, -1), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(5), 5)
+        self.assertEqual(multiply(-3), -3)
 
 if __name__ == '__main__':
     unittest.main()
